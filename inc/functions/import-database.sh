e7087960f036bfa6276766a985cf927077a3306c
Darklg <kev.rocher@gmail.com>
2016-01-08 22:25:06 +0100

    v 0.8.2
    * Ask if verbose import.

--- inc/functions/import-database.sh
+++ inc/functions/import-database.sh
@@ -9,7 +9,15 @@ if [[ $database_file_exists != '0' ]]; then
     database_file_size=$(du -h "${database_file_import}" | cut -f 1);
     read -p "Import database file '${database_file_import}' (${database_file_size} ) ? [Y/n]:" import_database;
     if [[ $import_database != 'n' ]]; then
-        mysql --verbose -u ${mysql_user} -p${mysql_pass} ${project_id} < ${database_file_import};
+
+        read -p "Verbose mode ? [y/N]:" import_database_verbose;
+
+        database_verbose_arg='';
+        if [[ $import_database != 'y' ]]; then
+            database_verbose_arg="--verbose";
+        fi;
+
+        mysql ${database_verbose_arg} -u ${mysql_user} -p${mysql_pass} ${project_id} < ${database_file_import};
         echo "-- Database imported from '${database_file_import}'";
     fi;
 fi;
