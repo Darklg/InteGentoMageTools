7a6b4ee511acf137e17d2952f62d9450bbe03c77
Kevin Rocher <kevin@rocher-dev.com>
2016-09-27 19:35:01 +0200

    v 0.21.1
    * Fix errors on Attribute Cat installer.
    * Add default fields.

--- files/tpl/update-attributecat.php
+++ files/tpl/update-attributecat.php
@@ -5,22 +5,26 @@
 
 try {
     /** @var Mage_Core_Model_Resource_Setup $installer */
+    $installer = $this;
     $installer->startSetup();
 
     /* Custom Attributes */
     $_customAttributes = array(
-        'custom_attribute' => array(
-            'label' => 'Custom Select',
+        /* Yes No */
+        'custom_select_yesno' => array(
             'input' => 'select',
-            'note' => 'Help text',
+            'label' => 'Custom Select',
+            'note' => 'Help text under',
+            'source' => 'eav/entity_attribute_source_boolean',
             'type' => 'int', // int / text / etc
-            'option' => array(
-                'values' => array(
-                    0 => 'White',
-                    1 => 'Black'
-                )
-            )
-        )
+        ),
+        /* Text field */
+        'custom_text' => array(
+            'input' => 'text',
+            'label' => 'Custom Text',
+            'note' => 'Help text under',
+            'type' => 'text', // int / text / etc
+        ),
     );
 
     /* Edit below at your own risks */
@@ -29,10 +33,12 @@ try {
         'backend' => '',
         'comparable' => false,
         'filterable' => false,
+        'global' => Mage_Catalog_Model_Resource_Eav_Attribute::SCOPE_STORE,
         'group' => 'General Information',
         'input' => 'text',
         'is_html_allowed_on_front' => false,
         'label' => 'Custom attribute',
+        'note' => '',
         'required' => false,
         'searchable' => false,
         'type' => 'text',
@@ -40,7 +46,6 @@ try {
         'visible' => true,
         'visible_on_front' => true,
         'wysiwyg_enabled' => false,
-        'global' => Mage_Catalog_Model_Resource_Eav_Attribute::SCOPE_STORE
     );
 
     foreach ($_customAttributes as $_id => $_attr) {
--- magetools.sh
+++ magetools.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Mage Tools v 0.21
+# Mage Tools v 0.21.1
 #
 # @author      Darklg <darklg.blog@gmail.com>
 # @copyright   Copyright (c) 2016 Darklg
